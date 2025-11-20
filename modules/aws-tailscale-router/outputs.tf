0c384009ef7be439e2f472a1721890d6c2a4307f
AshminPY <ashmin.py@gmail.com>
2025-11-20 10:09:04 -0500

    feat(aws): adding more outputs

--- modules/aws-tailscale-router/outputs.tf
+++ modules/aws-tailscale-router/outputs.tf
@@ -15,3 +15,33 @@ output "private_subnet_id" {
   description = "The private subnet ID"
   value       = module.aws_network.private_subnets[0]
 }
+
+# Internet Gateway ID
+output "igw_id" {
+  description = "Internet Gateway for public subnet"
+  value       = module.aws_network.igw_id
+}
+
+# NAT Gateway ID
+output "nat_gateway_id" {
+  description = "NAT Gateway for private subnet"
+  value       = module.aws_network.natgw_ids[0]
+}
+
+# EIP for NAT Gateway
+output "nat_eip" {
+  description = "Public Elastic IP for the NAT Gateway"
+  value       = module.aws_network.nat_public_ips[0]
+}
+
+# Public route table
+output "public_route_table_id" {
+  description = "Public route table ID"
+  value       = module.aws_network.public_route_table_ids[0]
+}
+
+# Private route table
+output "private_route_table_id" {
+  description = "Private route table ID"
+  value       = module.aws_network.private_route_table_ids[0]
+}
